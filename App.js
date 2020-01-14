027c0dd5196fbbb9c4719b0d158355028bc768de
vorti <vorti@vortiui-MacBookPro.local>
2020-01-14 14:26:12 +0900

    fuckin aws

--- App.js
+++ App.js
@@ -1,114 +1,124 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
-import React from 'react';
+import React,{
+  useState
+} from 'react';
 import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
   View,
   Text,
-  StatusBar,
+  TextInput,
+  TouchableOpacity,
+  FlatList
 } from 'react-native';
+import gql from 'graphql-tag'
+import { graphql } from 'react-apollo'
+// import { useQuery } from '@apollo/react-hooks';
+// import { useQuery } from 'react-apollo-hooks'
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+// import Amplify, { API, graphqlOperation } from "aws-amplify";
+// import * as mutations from './src/graphql/mutations';
+import * as queries from './src/graphql/queries';
 
-const App: () => React$Node = () => {
+// const getList = () => {
+//   const query = useQuery(gql(queries.listTodos), {
+//     fetchPolicy: 'cache-and-network',
+//   });
+//   return query;
+// };
+
+// const listTodos = gql(queries.listTodos)
+
+// export default (props) => {
+const App = () => {
+
+  const [ inputText, setInputText ] = useState("")
+  // const newTodo = await API.graphql(graphqlOperation(mutations.createTodo, {input: {name:'I', description:"inputText"}}))
+  // console.log(newTodo)
+  const [ listTodo, setListTodo ] = useState([])
+  // console.log(getList())
+  // API.graphql(graphqlOperation(queries.listTodos)).then(response =>setListTodo(response.data.listTodos.items))
+  btnPressed=()=>{
+    // console.log('sgasg')
+    // API.graphql(graphqlOperation(mutations.createTodo, {input: {name:'fromApp', description:inputText}}))
+  }
+
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
+    <View
+      style={{
+        flex:1
+      }}
+    >
+      <View
+        style={{
+          flex:1,
+          flexDirection:'row',
+          padding:5
+        }}
+      >
+        <TextInput
+          style={{
+            flex:4,
+            borderWidth:1
+          }}
+          onChangeText={text => setInputText(text)}
+        />
+        <TouchableOpacity
+          style={{
+            flex:1,
+            alignItems:'center',
+            justifyContent:'center'
+          }}
+          onPress={() => btnPressed()}
+        >
+          <Text>put</Text>
+        </TouchableOpacity>
+      </View>
+      <View
+        style={{
+          flex:9
+        }}
+      >
+        <FlatList
+          data={listTodo}
+          renderItem={({item}) => (
+            <View
+              style={{
+                width:'100%',
+                height:50,
+                flexDirection:'row',
+                borderBottomWidth:1
+              }}
+            >
+              <View
+                style={{
+                  flex:1,
+                  borderRightWidth:1,
+                  justifyContent:'center',
+                  alignItems:'center'
+                }}
+              >
+                <Text>{item.name}</Text>
+              </View>
+              <View
+                style={{
+                  flex:5,
+                  justifyContent:'center',
+                  alignItems:'center'
+                }}
+              >
+                <Text>{item.description}</Text>
+              </View>
             </View>
           )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+        />
+      </View>
+    </View>
   );
 };
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+export default graphql(gql(queries.listTodos), {
+  options: {
+    fetchPolicy: 'cache-and-network'
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
-
-export default App;
+  props: props => ({
+    todos: props.data.listTodos ? props.data.listTodos.items : []
+  })
+})(App)
--- index.js
+++ index.js
@@ -1,11 +1,54 @@
 /**
  * @format
  */
-
+import React, { useState, useEffect } from 'react';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
-import awsconfig from './src/aws-exports'
+import Amplify from 'aws-amplify'
+import AWSAppSyncClient, { buildSubscription } from 'aws-appsync';
+// import { Rehydrated, graphqlMutation } from 'aws-appsync-react';
+import { ApolloProvider, useApolloClient } from 'react-apollo';
+// import { ApolloProvider as ApolloHooksProvider } from '@apollo/react-hooks';
+// import {ApolloProvider as ApolloHooksProvider } from 'react-apollo-hooks'
+import AppSyncConfig from './src/aws-exports'
 
-AppRegistry.registerComponent(appName, () => App);
+Amplify.configure(AppSyncConfig)
+
+const client = new AWSAppSyncClient({
+    url: AppSyncConfig.aws_appsync_graphqlEndpoint,
+    region: AppSyncConfig.aws_appsync_region,
+    auth: {
+      type: AppSyncConfig.aws_appsync_authenticationType,
+      apiKey: AppSyncConfig.aws_appsync_apiKey,
+      // jwtToken: async () => token, // Required when you use Cognito UserPools OR OpenID Connect. token object is obtained previously
+    }
+  })
+
+const Rehydrated = ({ children }) => {
+    const client = useApolloClient();
+    const [rehydrated, setState] = useState(false);
+  
+    useEffect(() => {
+      if (client instanceof AWSAppSyncClient) {
+        (async () => {
+          await client.hydrated();
+          setState(true);
+        })();
+      }
+    }, [client]);
+  
+    return rehydrated ? children : null;
+  };
+  
+const WithProvider = () => (
+    <ApolloProvider client={client}>
+      {/* <ApolloHooksProvider client={client}> */}
+        <Rehydrated>
+          <App />
+        </Rehydrated>
+      {/* </ApolloHooksProvider> */}
+    </ApolloProvider>
+  )
+AppRegistry.registerComponent(appName, () => WithProvider);
